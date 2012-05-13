23ada69c7a7847d09a53accfb647ad55d8158e30
Jaeho Shin <netj@sparcs.org>
2012-05-12 18:12:50 -0700

    Polished messages, show warning of variable source paths
    - concise message display about BuildKit steps with prefix `### BuildKit:`
    - glob patterns used as source in .module.install files can cause
      incorrect builds when files matching them are added or removed, so warning
      was required

--- modules.mk
+++ modules.mk
@@ -5,7 +5,8 @@
 BUILDKIT?=$(shell pwd)/buildkit
 SHELL:=$(shell which bash)
 PATH:=$(BUILDKIT):$(PATH)
-export SHELL PATH
+CDPATH:=
+export SHELL PATH CDPATH
 
 STAGEDIR?=.stage
 BUILDDIR?=.build
@@ -18,28 +19,32 @@ MODULES:=$(shell $(BUILDKIT)/all-modules | $(BUILDKIT)/order-by-depends)
 export PREFIX
 
 
-.PHONY: all build stage clean package install
+.PHONY: all build index stage clean package install
 
 all: stage
 
+stage: build
 
 build:
 	@\
 	    buildable-modules $(MODULES) | \
 	    xargs modified-modules $(BUILD_TIMESTAMP_FMT) | \
-	    xargs build-modules $(BUILD_TIMESTAMP_FMT)
-# TODO we'll soon need to consider dependencies between modules :(
-
-
-stage: build
+	    xargs build-modules $(BUILD_TIMESTAMP_FMT) \
+	    # TODO we'll soon need to consider dependencies between modules :(
 ifndef BUILD_BEFORE_STAGING
+	### BuildKit: built all modules
+
 include $(BUILDDIR)/stage.mk
+index \
 $(BUILDDIR)/stage.mk: $(BUILDKIT)/generate-staging-rules $(BUILDKIT)/modules.mk $(MODULES:%=%/.module.install)
-	mkdir -p $(@D)
-	# we need to first build before we determine staging rules
-	$(MAKE) BUILD_BEFORE_STAGING=required build
-	STAGEDIR=$(STAGEDIR) BUILDDIR=$(BUILDDIR) \
-generate-staging-rules >$@ $(MODULES)
+	### BuildKit: generating staging rules
+	@mkdir -p $(BUILDDIR)
+	@# To determine correct staging rules, we need a prior build
+	@$(MAKE) BUILD_BEFORE_STAGING=required build
+	@\
+	    STAGEDIR=$(STAGEDIR) BUILDDIR=$(BUILDDIR) \
+	    generate-staging-rules >$(BUILDDIR)/stage.mk $(MODULES) \
+	    #
 endif
 
 
@@ -48,28 +53,35 @@ ifdef PACKAGEEXECUTES
 PACKAGE := $(PACKAGENAME)-$(PACKAGEVERSION).sh
 package: $(PACKAGE)
 $(PACKAGE): stage
-	PACKAGENAME=$(PACKAGENAME) \
-	pojang $@ $(STAGEDIR) $(PACKAGEEXECUTES) .
+	@\
+	    PACKAGENAME=$(PACKAGENAME) \
+	    pojang $@ $(STAGEDIR) $(PACKAGEEXECUTES) . \
+	    #
+	### BuildKit: packaged as $(PACKAGE)
 
 # we want to do create the package everytime
 all: package
 
 install: $(PACKAGE)
-	mkdir -p $(PREFIX)/bin
-	install $< $(PREFIX)/bin/$(PACKAGENAME)
+	### BuildKit: installing at $(PREFIX)/bin/$(PACKAGENAME)
+	@mkdir -p $(PREFIX)/bin
+	@install $< $(PREFIX)/bin/$(PACKAGENAME)
 else
 # otherwise, just create an ordinary tarball
 PACKAGE := $(PACKAGENAME)-$(PACKAGEVERSION).tar.gz
 package: $(PACKAGE)
 $(PACKAGE): stage
-	tar czvf $@ -C $(STAGEDIR) .
+	@tar czf $@ -C $(STAGEDIR) .
+	### BuildKit: packaged as $(PACKAGE)
 
 install: $(PACKAGE)
-	mkdir -p $(PREFIX)
-	tar xzvf $< -C $(PREFIX)
+	### BuildKit: installing at $(PREFIX)
+	@mkdir -p $(PREFIX)
+	@tar xzf $< -C $(PREFIX)
 endif
 
 
 clean:
-	rm -rf $(STAGEDIR) $(BUILDDIR) $(PACKAGE)
+	@rm -rf $(STAGEDIR) $(BUILDDIR) $(PACKAGE)
+	### BuildKit: cleaned
 
