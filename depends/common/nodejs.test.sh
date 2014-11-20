a544c589b28157df553ec00075aa79723d5b935b
Jaeho Shin <netj@cs.stanford.edu>
2014-11-19 16:33:37 -0800

    Made nodejs version checking more robust

--- depends/common/nodejs.test.sh
+++ depends/common/nodejs.test.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
+set -eu
 
-# check nodejs version
+# the version we're looking for
 version=${DEPENDS_ON_NODE_VERSION:-v0.10.26}
 
-if ! type node npm &>/dev/null || [[ $(node --version) < $version ]]; then
-    echo >&2 "nodejs >= $version not found"
+# check if nodejs is here
+type node npm &>/dev/null || {
+    echo >&2 "nodejs not found"
     false
-fi
+}
+
+# compare versions
+versionHere=$(node --version)
+vTail=${version#v}           vTailHere=${versionHere#v}
+while [[ -n "$vTail" && -n "$vTailHere" ]]; do
+    vHead=${vTail%%.*}    vHeadHere=${vTailHere%%.*}
+    vTail=${vTail#$vHead} vTailHere=${vTailHere#$vHeadHere}
+    vTail=${vTail#.}      vTailHere=${vTailHere#.}
+    [[ $vHead -ge $vHeadHere ]] || {
+        echo >&2 "nodejs >= $version not found"
+        false
+    }
+done
+
+true
