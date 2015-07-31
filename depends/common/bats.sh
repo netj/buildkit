c04c0dfb1016ac1474f16abac17022cf7f9c2dd9
Jaeho Shin <netj@sparcs.org>
2015-07-30 17:32:18 -0700

    Makes it possible to tweak BATS version

--- depends/common/bats.sh
+++ depends/common/bats.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -eu
 
+branch=${DEPENDS_ON_BATS_VERSION:-v0.4.0}
+
 rm -rf bats
-git clone https://github.com/sstephenson/bats.git --branch v0.3.1
+git clone https://github.com/sstephenson/bats.git --branch $branch
 cd bats
 ./install.sh "$DEPENDS_PREFIX"
