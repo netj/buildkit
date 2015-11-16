9143c26a965ef46d5a0c9fbac4cd011575060816
Jaeho Shin <netj@cs.stanford.edu>
2015-11-16 11:48:00 -0800

    Improves fetch-verify's error message, updates bash to 4.3

--- depends/common/bash/install.sh
+++ depends/common/bash/install.sh
@@ -3,14 +3,16 @@
 set -eu
 
 name=bash
-version=4.2
-sha1sum=487840ab7134eb7901fbb2e49b0ee3d22de15cb8
-md5sum=3fb927c7c33022f1c327f14a81c0d4b0
+version=4.3
+sha256sum=afc687a28e0e24dc21b988fa159ff9dbcf6b7caa92ade8645cc6d5605cd024d4
+sha1sum=45ac3c5727e7262334f4dfadecdf601b39434e84
+md5sum=81348932d5da294953e15d4814c74dd1
 ext=.tar.gz
 
-patchesURL=https://gist.github.com/jacknagel/4008180/raw/1509a257060aa94e5349250306cce9eb884c837d/bash-4.2-001-045.patch
-patches_sha1sum=f10d42cf4a7bc6d5599d705d270a602e02dfd517
-patches_md5sum=e5af015e91dd5aaf07a5208623f5649a
+patchesURL=https://gist.githubusercontent.com/dunn/a8986687991b57eb3b25/raw/76dd864812e821816f4b1c18e3333c8fced3919b/bash-4.3.42.diff
+patches_sha256sum=2eeb9b3ed71f1e13292c2212b6b8036bc258c58ec9c82eec7a86a091b05b15d2
+patches_sha1sum=982e8ff0cca3c4e92a59336a4eb22cee4a5b4100
+patches_md5sum=749f45b46057f18111f0422da34ca0dc
 
 fetch-configure-build-install $name-$version <<END
 url=http://ftpmirror.gnu.org/$name/$name-$version$ext
@@ -23,7 +25,7 @@ custom-fetch() {
     fetch-verify "\$patchesName" "$patchesURL" \
         sha1sum=$patches_sha1sum md5sum=$patches_md5sum
     cd "$name-$version"
-    patch -p0 <../"\$patchesName"
+    patch -p1 <../"\$patchesName"
 }
 custom-install() { make install-strip; }
 END
