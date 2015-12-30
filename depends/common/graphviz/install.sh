a6fba0e5ebd7d04749ebd467d20b4769858d9171
Jaeho Shin <netj@cs.stanford.edu>
2015-12-29 17:20:47 -0800

    Slims down graphviz depends build
    by disabling X and Tcl and installing only executables

--- depends/common/graphviz/install.sh
+++ depends/common/graphviz/install.sh
@@ -10,4 +10,11 @@ fetch-configure-build-install $name-$version <<END
 url=http://graphviz.org/pub/graphviz/stable/SOURCES/graphviz-${version}.tar.gz
 sha1sum=$sha1sum
 md5sum=$md5sum
+custom-configure() {
+    default-configure --disable-tcl --without-x
+}
+custom-install() {
+    # don't install docs and data, just the executable binaries
+    make install-exec
+}
 END
