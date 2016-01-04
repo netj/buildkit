588da096d9af96f7e97be5e53c68729814297fc1
Jaeho Shin <netj@cs.stanford.edu>
2016-01-04 01:51:48 -0800

    Switches graphviz URL to a more stable source

--- depends/common/graphviz/install.sh
+++ depends/common/graphviz/install.sh
@@ -3,11 +3,12 @@ set -eu
 
 name=graphviz
 version=2.38.0
-sha1sum=
-md5sum=
+sha1sum=053c771278909160916ca5464a0a98ebf034c6ef
+md5sum=5b6a829b2ac94efcd5fa3c223ed6d3ae
 
 fetch-configure-build-install $name-$version <<END
-url=http://graphviz.org/pub/graphviz/stable/SOURCES/graphviz-${version}.tar.gz
+#url=http://graphviz.org/pub/graphviz/stable/SOURCES/graphviz-${version}.tar.gz
+url=http://pkgs.fedoraproject.org/repo/pkgs/graphviz/graphviz-${version}.tar.gz/${md5sum}/graphviz-${version}.tar.gz
 sha1sum=$sha1sum
 md5sum=$md5sum
 custom-configure() {
