1120c30dc47f5d72566a34ad94f52910e4f5b590
Jaeho Shin <netj@cs.stanford.edu>
2016-01-11 01:42:24 -0800

    Adds pbzip2 dependency recipe

---
+++ depends/common/pbzip2/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -eu
+
+name=pbzip2
+version=1.1.13
+sha1sum=f61e65a7616a3492815d18689c202d0685fe167d
+md5sum=4cb87da2dba05540afce162f34b3a9a6
+ext=.tar.gz
+
+fetch-configure-build-install $name-$version <<END
+url=https://launchpad.net/$name/${version%.*}/$version/+download/$name-$version$ext
+sha1sum=$sha1sum
+md5sum=$md5sum
+custom-configure() { :; }
+custom-install() { default-install PREFIX=$DEPENDS_PREFIX; }
+END
