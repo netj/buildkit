4aeb259ccb2b435cb45aca7f64e60cf4f5196f6a
Jaeho Shin <netj@cs.stanford.edu>
2016-03-18 11:33:42 -0700

    Updates moreutils to 0.58
    since 0.57 is gone from Debian mirrors

--- depends/common/moreutils/install.sh
+++ depends/common/moreutils/install.sh
@@ -2,9 +2,9 @@
 set -eu
 
 name=moreutils
-version=0.57
-sha1sum=1b53b6669a414f50ea2ec708f4e9d72ad703c784
-md5sum=2fd82a15dea059506a6f43ce717dbfad
+version=0.58
+sha1sum= #1b53b6669a414f50ea2ec708f4e9d72ad703c784
+md5sum= #2fd82a15dea059506a6f43ce717dbfad
 
 fetch-configure-build-install $name-$version <<END
 url=https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/m/moreutils/moreutils_$version.orig.tar.gz
