731598e435f4bd201beea3be8511d45a382f3d02
Jaeho Shin <jaeho.shin@lattice.io>
2016-06-12 16:40:13 -0700

    Fixes moreutils to fetch from github clone, bumps to version 0.59

--- depends/common/moreutils/install.sh
+++ depends/common/moreutils/install.sh
@@ -2,12 +2,12 @@
 set -eu
 
 name=moreutils
-version=0.58
-sha1sum= #1b53b6669a414f50ea2ec708f4e9d72ad703c784
-md5sum= #2fd82a15dea059506a6f43ce717dbfad
+version=0.59
+sha1sum=7731c862384e05243e4856b462185152f87d31ab
+md5sum=718900463335f8a9405493c5db5cf4bb
 
 fetch-configure-build-install $name-$version <<END
-url=https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/m/moreutils/moreutils_$version.orig.tar.gz
+url=https://github.com/netj/moreutils/archive/$version.tar.gz
 sha1sum=$sha1sum
 md5sum=$md5sum
 custom-configure() { :; }
